825fdd2142e35a8ffa601d584d03e250cee93f2a
bala-amypo <bala@amypo.in>
2025-12-18 09:46:05 +0000

    User commit

--- src/main/java/com/example/demo/model/Stock.java
+++ src/main/java/com/example/demo/model/Stock.java
@@ -1,3 +1,31 @@
 public class Stock{
     
 }
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "ticker"))
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ticker;
+    private String companyName;
+    private String sector;
+    private Boolean active = true;
+
+    // getters & setters
+    public Long getId() { return id; }
+    public String getTicker() { return ticker; }
+    public void setTicker(String ticker) { this.ticker = ticker; }
+    public String getCompanyName() { return companyName; }
+    public void setCompanyName(String companyName) { this.companyName = companyName; }
+    public String getSector() { return sector; }
+    public void setSector(String sector) { this.sector = sector; }
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+}
