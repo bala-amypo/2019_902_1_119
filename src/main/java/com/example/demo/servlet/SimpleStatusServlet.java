adb02c320c8cff3970444ac29cb3194a99b717b2
bala-amypo <bala@amypo.in>
2025-12-25 06:34:44 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,22 +1,22 @@
-package com.example.demo.servlet;
+// package com.example.demo.servlet;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.annotation.WebServlet;
+// import jakarta.servlet.http.HttpServlet;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import java.io.IOException;
 
-@WebServlet(urlPatterns = "/status")
-public class SimpleStatusServlet extends HttpServlet {
+// @WebServlet(urlPatterns = "/status")
+// public class SimpleStatusServlet extends HttpServlet {
     
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        response.setStatus(200);
-        response.getWriter().write("Servlet Running!");
-    }
-}
+//     @Override
+//     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+//             throws ServletException, IOException {
+//         response.setStatus(200);
+//         response.getWriter().write("Servlet Running!");
+//     }
+// }
 
 
 
